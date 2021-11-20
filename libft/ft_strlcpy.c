982843bc5a99394b1fe2d63a3e7118d832376752
Youngsoo Hwang <yohwang@c1r2s3.42seoul.kr>
2021-11-20 17:54:47 +0900

    ft_strlcpy (yet)

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,8 +6,33 @@
 /*   By: yohwang <yohwang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 16:40:24 by yohwang           #+#    #+#             */
-/*   Updated: 2021/11/20 16:48:35 by yohwang          ###   ########.fr       */
+/*   Updated: 2021/11/20 17:53:24 by yohwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
 
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
+{
+	size_t	i;
+
+	if (dstsize == 0)
+		return (ft_strlen(src));
+	i = 0;
+	while (i < dstsize)
+	{
+		dst[i] = src[i];
+		if (src[i] == '\0')
+			break;
+		i++;
+	}
+	dst[i] = '\0';
+	return (ft_strlen(src));
+}
+
+int main()
+{
+	char src[] = "coucou";
+	char dest[10] = "AAAAAAAAAA";
+	size_t i = ft_strlcpy(dest, src, 0);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: yohwang <yohwang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 15:34:32 by yohwang           #+#    #+#             */
-/*   Updated: 2021/11/20 13:53:41 by yohwang          ###   ########.fr       */
+/*   Updated: 2021/11/20 17:51:47 by yohwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_memcmp(const void *s1, const void *s2, size_t n);
 
 size_t	ft_strlen(const char *s);
-
+size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
 
