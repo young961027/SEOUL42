b4f24dae1e1b0c9a7ae74012b4c7af9b99e51386
Youngsoo Hwang <yohwang@c1r2s3.42seoul.kr>
2021-11-20 16:49:15 +0900

    Header error

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -1,37 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_memcmp.c                                        :+:      :+:    :+:   */
+/*   ft_strlcpy.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: yohwang <yohwang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/11/20 16:43:56 by yohwang           #+#    #+#             */
-/*   Updated: 2021/11/20 16:47:44 by yohwang          ###   ########.fr       */
+/*   Created: 2021/11/20 16:40:24 by yohwang           #+#    #+#             */
+/*   Updated: 2021/11/20 16:48:35 by yohwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 
-#include "libft.h"
-#include <stdio.h>
-
-int	ft_memcmp(const void *s1, const void *s2, size_t n)
-{
-	unsigned char	*c1;
-	unsigned char	*c2;
-	int				len1;
-	int				len2;
-	size_t			i;
-
-	c1 = (unsigned char *)s1;
-	c2 = (unsigned char *)s2;
-	len1 = 0;
-	len2 = 0;
-	i = 0;
-	while (i < n)
-	{
-		len1 += (int)c1[i];
-		len2 += (int)c2[i];
-		i++;
-	}
-	return (len1 - len2);
-}
