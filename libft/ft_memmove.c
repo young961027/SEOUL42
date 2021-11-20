ab43f405b4dc908c7824cdc7a3203753dab0e136
Youngsoo Hwang <yohwang@c1r2s3.42seoul.kr>
2021-11-20 16:39:14 +0900

    ft_memmove

---
+++ libft/ft_memmove.c
@@ -0,0 +1,40 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_memmove.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yohwang <yohwang@student.42seoul.k>        +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2021/11/20 13:48:08 by yohwang           #+#    #+#             */
+/*   Updated: 2021/11/20 16:37:57 by yohwang          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+void	*ft_memmove(void *dst, const void *src, size_t len)
+{
+	unsigned char	*cdst;
+	unsigned char	*csrc;
+	size_t			i;
+
+	cdst = (unsigned char *)dst;
+	csrc = (unsigned char *)src;
+	if (dst > src)
+	{
+		i = len;
+		while (i > 0)
+		{
+			cdst[i - 1] = csrc[i - 1];
+			i--;
+		}
+		return ((void *)cdst);
+	}
+	i = 0;
+	while (i < len)
+	{
+		cdst[i] = csrc[i];
+		i++;
+	}
+	return ((void *)cdst);
+}
--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: yohwang <yohwang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/12 15:34:32 by yohwang           #+#    #+#             */
-/*   Updated: 2021/11/19 20:15:57 by yohwang          ###   ########.fr       */
+/*   Updated: 2021/11/20 13:53:41 by yohwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -34,4 +34,5 @@ void	ft_bzero(void *s, size_t n);
 void	*ft_memchr(const void *s, int c, size_t n);
 void	*ft_memset(void *b, int c, size_t len);
 void	*ft_memcpy(void *dst, const void *src, size_t n);
+void	*ft_memmove(void *dst, const void *src, size_t len);
 #endif
