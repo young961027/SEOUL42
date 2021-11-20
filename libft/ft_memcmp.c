933cf72a45bbbc52acdecbba23227cf8ea8fc5a3
Youngsoo Hwang <yohwang@c1r2s3.42seoul.kr>
2021-11-20 13:47:57 +0900

    ft_memcmp

--- libft/ft_memcmp.c
+++ libft/ft_memcmp.c
@@ -6,7 +6,7 @@
 /*   By: yohwang <yohwang@student.42seoul.k>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/19 17:14:01 by yohwang           #+#    #+#             */
-/*   Updated: 2021/11/19 20:56:08 by yohwang          ###   ########.fr       */
+/*   Updated: 2021/11/20 13:46:59 by yohwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,14 +15,14 @@
 
 int	ft_memcmp(const void *s1, const void *s2, size_t n)
 {
-	char	*c1;
-	char	*c2;
-	int		len1;
-	int		len2;
-	size_t	i;
+	unsigned char	*c1;
+	unsigned char	*c2;
+	int				len1;
+	int				len2;
+	size_t			i;
 
-	c1 = (char *)s1;
-	c2 = (char *)s2;
+	c1 = (unsigned char *)s1;
+	c2 = (unsigned char *)s2;
 	len1 = 0;
 	len2 = 0;
 	i = 0;
@@ -34,17 +34,3 @@ int	ft_memcmp(const void *s1, const void *s2, size_t n)
 	}
 	return (len1 - len2);
 }
-/*
-int main()
-{
-	char s[] = {-128, 0, 127, 0};
-	char s1[] = {-128, 0, 127, 0};
-	char s2[] = {0,0,127,0};
-	char s3[] = {0,0,42,0};
-	int r1 = ft_memcmp(s,s1,4);
-	int r2 = ft_memcmp(s,s2,0);
-	int r3 = ft_memcmp(s, s2, 1);
-	int r4 = ft_memcmp(s2, s, 1);
-	int r5 = ft_memcmp(s2, s3, 4);
-	printf("\n %d %d %d %d %d \n", r1, r2, r3, r4, r5);
-}*/
