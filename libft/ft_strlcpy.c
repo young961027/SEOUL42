19390062cdc9cffdfa89a9de19074889a5c17a8f
Youngsoo Hwang <yohwang@c1r2s3.42seoul.kr>
2021-11-22 13:29:14 +0900

    ft_strlcpy

--- libft/ft_strlcpy.c
+++ libft/ft_strlcpy.c
@@ -6,7 +6,7 @@
 /*   By: yohwang <yohwang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/20 16:40:24 by yohwang           #+#    #+#             */
-/*   Updated: 2021/11/20 17:53:24 by yohwang          ###   ########.fr       */
+/*   Updated: 2021/11/22 13:28:58 by yohwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	if (dstsize == 0)
 		return (ft_strlen(src));
 	i = 0;
-	while (i < dstsize)
+	while (i < dstsize - 1)
 	{
 		dst[i] = src[i];
 		if (src[i] == '\0')
@@ -28,11 +28,4 @@ size_t	ft_strlcpy(char *dst, const char *src, size_t dstsize)
 	}
 	dst[i] = '\0';
 	return (ft_strlen(src));
-}
-
-int main()
-{
-	char src[] = "coucou";
-	char dest[10] = "AAAAAAAAAA";
-	size_t i = ft_strlcpy(dest, src, 0);
 }
