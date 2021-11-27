be7f32d6c511e4a0b8bdfcef1e970117680940de
young961027 <young961027@gmail.com>
2021-11-27 21:58:24 +0900

    ft_split not yet

--- libft/ft_split.c
+++ libft/ft_split.c
@@ -6,13 +6,13 @@
 /*   By: yohwang <yohwang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/26 18:42:24 by yohwang           #+#    #+#             */
-/*   Updated: 2021/11/26 21:28:12 by yohwang          ###   ########.fr       */
+/*   Updated: 2021/11/27 21:54:36 by yohwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-size_t	count_char(char const *s, char c)
+static size_t	count_char(char const *s, char c)
 {
 	size_t	i;
 	size_t	count;
@@ -28,13 +28,15 @@ size_t	count_char(char const *s, char c)
 	return (count);
 }
 
-char	*fill_word(char const *s, size_t i, size_t j, char *word)
+static char	*fill_word(char const *s, size_t i, size_t j)
 {
+	cha
 	size_t	idx;
 	size_t	len;
 
 	idx = 0;
 	len = i - j;
+
 	while (idx < len)
 	{
 		word[idx] = s[idx + j];
@@ -63,6 +65,8 @@ char	**ft_split(char const *s, char c)
 		if ((int)(i - j) > 0)
 		{
 			word = malloc(i - j + 1);
+			if (!word)
+				return (0);
 			word = fill_word(s, i, j, word);
 			tab[count] = word;
 			count++;
@@ -71,4 +75,4 @@ char	**ft_split(char const *s, char c)
 	}
 	tab[count] = 0;
 	return (tab);
-}
+}	
