dbca5f6db478cbeab6c725abb2544d0d6a6a007e
young961027 <young961027@gmail.com>
2021-11-27 16:14:48 +0900

    strncmp UT

--- libft/ft_strncmp.c
+++ libft/ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: yohwang <yohwang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 16:29:03 by yohwang           #+#    #+#             */
-/*   Updated: 2021/11/12 15:58:29 by yohwang          ###   ########.fr       */
+/*   Updated: 2021/11/27 16:14:26 by yohwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,17 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	unsigned char	*c1;
+	unsigned char	*c2;
+	size_t			i;
 
+	c1 = (unsigned char *)s1;
+	c2 = (unsigned char *)s2;
 	i = 0;
 	while (i < n)
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (c1[i] != c2[i] || c1[i] == '\0' || c2[i] == '\0')
+			return (c1[i] - c2[i]);
 		else
 			i++;
 	}
