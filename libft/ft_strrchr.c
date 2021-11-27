e0389919d3d9ac0d672ce1b63527b0e79853867f
young961027 <young961027@gmail.com>
2021-11-27 16:18:08 +0900

    strrchr UT

--- libft/ft_strrchr.c
+++ libft/ft_strrchr.c
@@ -6,7 +6,7 @@
 /*   By: yohwang <yohwang@student.42seoul.kr>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/11/11 15:18:17 by yohwang           #+#    #+#             */
-/*   Updated: 2021/11/27 15:53:39 by yohwang          ###   ########.fr       */
+/*   Updated: 2021/11/27 16:16:42 by yohwang          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,10 +24,12 @@ char	*ft_strrchr(const char *s, int c)
 		if (s[i] == (char)c)
 			return ((char *)s + i);
 		else
+		{
 			if (i == 0)
 				return (0);
 			else
 				i--;
+		}
 	}
 	return (0);
 }
